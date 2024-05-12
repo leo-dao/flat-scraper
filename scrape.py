ed1002f33bfbe96e8ddb306fd038b3a14783093b
Leo Dao <leotomdao@gmail.com>
2024-05-12 16:36:56 -0400

    Ensure page loads correctly + check for elements existing

--- scrape.py
+++ scrape.py
@@ -4,13 +4,14 @@ from splinter import Browser
 import time
 
 def close_popup(browser):
-    if browser.is_element_present_by_css('div[aria-label="Close"]', wait_time=10):
+    time.sleep(5)
+    if browser.is_element_present_by_css('div[aria-label="Close"]'):
         # Click on the element once it's found
         browser.find_by_css('div[aria-label="Close"]').first.click()
 
 def goto_page(browser, url):
     browser.visit(url)
-    time.sleep(3)
+    time.sleep(1)
     close_popup(browser)
 
 def scroll_down(browser):
@@ -29,21 +30,25 @@ def getPrice(listing):
     return price
 
 def getListingInfo(browser, html):
-    listing_soup = BeautifulSoup(html, 'html.parser')
+    
+    # Scroll a bit to load the description
+    browser.execute_script("window.scrollTo(0, 500);")
 
-    # Click on "See More" button to get full description
     class_name = "x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1n2onr6 x87ps6o x1lku1pv x1a2a7pz"
     selector = f'div[class="{class_name}"][role="button"]:not([aria-label])'
 
-    button = browser.find_by_css(selector).first
+    time.sleep(5)
 
-    print(button.outer_html)
+    if browser.is_element_present_by_css(selector):
 
-    if button:
+        button = browser.find_by_css(selector).first
 
         button.click()
 
-        description = browser.find_by_css('span[class="x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u"]')[1].text
+    descriptionSelector = 'span[class="x193iq5w xeuugli x13faqbe x1vvkbs xlh3980 xvmahel x1n0sxbx x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u"]'
+
+    if browser.is_element_present_by_css(descriptionSelector):
+        description = browser.find_by_css(descriptionSelector)[1].text
         print(description)
 
 
@@ -75,17 +80,13 @@ def __main__():
         price = getPrice(listing)
 
         # TODO: User input for max price
-        # If the price is greater than the max price, skip the listing
-        max_price = 2000
-        min_price = 1000
-        if price > max_price or price < min_price:
-            continue
         
         # Get the link of the listing
         link = listing.get('href')
         
         goto_page(browser, base_url + link)
 
+        print(f"Scraping listing: {link}")
         listing_info = getListingInfo( browser, html)
 
         # Append the listing to the list
