67b285be0d35d0c5ff4054bcce9a8f5477dfefe5
Leo Dao <leotomdao@gmail.com>
2024-05-30 12:12:39 -0400

    Created user and post models

--- backend/DataProcessor/models.py
+++ backend/DataProcessor/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True) 
+    posts = models.ManyToManyField('Post', through='UserPost')
+
+
+    def __str__(self):
+        return self.email
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField(unique=True)
+    date_scraped = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class UserPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_sent = models.DateTimeField(auto_now_add=True)
